424c2d9696a6ea6451f4d19da66da9c041de9a48
Emengkeng <blessedsonlove@gmail.com>
2025-12-12 12:50:47 +0100

    refactor: make indexer the source of truth

--- src/indexer/indexer.module.ts
+++ src/indexer/indexer.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { IndexerService } from './indexer.service';
 import { EventParserService } from './event-parser.service';
 import { SolanaService } from './solana.service';
+import { WebhookService } from '../webhook/webhook.service';
+import { CheckoutService } from '../checkout/checkout.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  providers: [IndexerService, EventParserService, SolanaService],
+  imports: [HttpModule],
+  providers: [
+    IndexerService,
+    EventParserService,
+    SolanaService,
+    WebhookService,
+    CheckoutService,
+  ],
   exports: [IndexerService, SolanaService],
 })
 export class IndexerModule {}
